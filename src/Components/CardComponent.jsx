677416bdccf14334af67b6b4b69166745f035cda
kececirecep <kececirecep@outlook.com>
2023-11-26 01:45:58 +0300

    update

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -7,7 +7,7 @@ const CardComponent = (props) => {
     return (
         <div className='overflow-hidden bg-white/5 rounded-md border-2 border-transparent hover:bg-slate-800 hover:-translate-y-5 duration-300 hover:shadow-gray-400 hover:border-blue-600'>
             <NavLink to={props.link} target='_blank'>
-                <img src={props.image} alt="" className='w-full object-cover mb-4 ' /> 
+                <img src={props.image} alt="" className='w-full h-[230px] object-cover mb-4 ' /> 
                 <h3 className='text-base line-clamp-1 lg:text-xl mb-4 text-center'>{props.h3}</h3>
             </NavLink>
             <div className='flex justify-center items-center gap-4 pb-8'>
