c13770f3f41b19df7c5a7d550e2e3a294b4c3b8f
Recep Kececi <97679666+kececirecep@users.noreply.github.com>
2023-11-30 23:59:25 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,9 +23,9 @@ const Home = () => {
         <h1 className='text-xl lg:text-4xl font-bold lg:leading-[46px]'>Web Günceme,<br />Hoşgeldin!</h1>
         <p className='text-[15px] lg:text-lg text-gray-400 py-4'>Bu yazıda projelerimle ilgili deneyimlerimi ve başarılarımı paylaşmak istiyorum. Profesyonel kariyerimde çeşitli projelerde yer aldım ve bu projelerde kazandığım deneyimlerle kendimi sürekli geliştirmekteyim.</p>
         <div className="flex items-center gap-x-4 mt-5 text-gray-400 text-[22px]">
-          <Link to={`https://x.com/${myGithub.data.twitter_username}`} className="hover:text-white transition-colors"><BsTwitter /></Link>
-          <Link to="https://github.com/kececirecep" className="hover:text-white transition-colors"><BsGithub /></Link>
-          <Link to="https://www.linkedin.com/in/recepkececi/" className="hover:text-white transition-colors"><BsLinkedin /></Link>
+          <Link to={`https://x.com/${myGithub.data.twitter_username}`} target="_blank" className="hover:text-white transition-colors"><BsTwitter /></Link>
+          <Link to="https://github.com/kececirecep" target="_blank" className="hover:text-white transition-colors"><BsGithub /></Link>
+          <Link to="https://www.linkedin.com/in/recepkececi/" target="_blank" className="hover:text-white transition-colors"><BsLinkedin /></Link>
         </div>
       </div>
     </div>
